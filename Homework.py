73e546cd0ef11d7f9866bf91c2c9726ce0811b6b
Neilk24 <neilk2050@gmail.com>
2024-09-16 19:22:22 -0400

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,48 @@
+import time
+input=input("Enter any character: ")
+characterList=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+if input in characterList:
+    print("Your letter is a character😁😁😁")
+else:
+    print("Womp Womp, your letter is not a character 😭😭😭")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(6.5)
+
+
+print("Hello teacher.")
+time.sleep(4.5)
+print("You are getting robbed")
+time.sleep(4.5)
+print("Give me $1,000,000 right now.")
+time.sleep(3.5)
+print("or else...")
+time.sleep(4.5)
+print("Say goodbye to Codingal")
+time.sleep(6)
+print("This is a prank by Neil!!🥳🥳🎉🎉")
